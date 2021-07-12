ec95c554b06842e2c9f6bd41c7eb4b4a3042cd88
gejo <gejo@gejoui-iMac.local>
2021-07-12 23:42:24 +0900

    0712

--- so_long/parsing.c
+++ so_long/parsing.c
@@ -6,28 +6,11 @@
 /*   By: gejo <gejo@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/11 23:34:14 by gejo              #+#    #+#             */
-/*   Updated: 2021/07/12 00:19:31 by gejo             ###   ########.fr       */
+/*   Updated: 2021/07/12 23:24:19 by gejo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
-
-typedef struct s_map
-{
-	int		x;
-	int		y;
-	int		c;
-	int		e;
-	int		p;
-	char	*m_str;
-}	t_map;
-
-typedef struct s_game
-{
-	int x;
-	int y;
-	char **map;
-}
 
 char *ft_map_str(char *map_file)
 {
@@ -37,6 +20,7 @@ char *ft_map_str(char *map_file)
 	char buff[2];
 	
 	m_str = NULL;
+	tmp = NULL;
 	fd = open(map_file, O_RDONLY);
 	while (read(fd, buff, 1) > 0)
 	{
@@ -46,7 +30,7 @@ char *ft_map_str(char *map_file)
 		m_str = NULL;
 		m_str = tmp;
 	}
-	printf("%s", m_str);
+	printf("%s", m_str);	// m_str print
 	return (m_str);
 }
 
@@ -65,6 +49,16 @@ int ft_check_1st_line(char *m_str)
 	return (1);
 }
 
+int ft_check_map_char(char c)
+{
+	if (c == '0' || c== '1')
+		return (1);
+	else if (c == 'C' || c == 'P' || c == 'E')
+		return (1);
+	else
+		return (0);
+}
+
 int ft_check_line(char *m_str)
 {
 	int i;
@@ -81,6 +75,8 @@ int ft_check_line(char *m_str)
 			if (valid_line != 1)
 				return (0);
 		}
+		else if (m_str[i] != '\n' && ft_check_map_char(m_str[i]) == 0)
+			return (0);
 		i++;
 	}
 	return (1);
@@ -156,7 +152,35 @@ int ft_map_close_check(t_map *map)
 	return (0);
 }
 
-t_map *ft_map_parse(char *map_file)
+int ft_exist_data(char *m_str)
+{
+	int c;
+	int e;
+	int p;
+	int i;
+	
+	c = 0;
+	e = 0;
+	p = 0;
+	i = 0;
+	while (m_str[i] != '\0')
+	{
+		if (m_str[i] == 'C')
+			c++;
+		else if (m_str[i] == 'E')
+			e++;
+		else if (m_str[i] == 'P')
+			p++;
+		i++;
+	}
+	if (p != 1)
+		return (0);
+	else if (c < 1 || e < 1)
+		return (0);
+	return (1);
+}
+
+t_map *ft_map_validation(char *map_file)
 {
 	t_map *map;
 
@@ -171,20 +195,186 @@ t_map *ft_map_parse(char *map_file)
 	}
 	if (ft_map_close_check(map) == 1)
 		printf("map valid check pass\n");
+	else
+	{
+		printf("map invalid\n");
+		exit(1);
+	}
+	if (ft_exist_data(map->m_str) == 1)
+		printf("game data valid check pass\n");
+	else
+	{
+		printf("game data invalid\n");
+		exit(1);
+	}
 	return (map);
+}
+
+void ft_init_map_xy(t_game *game, char *m_str)
+{
+	int i;
+	int line;
+
+	i = 0;
+	while (m_str[i] != '\n')
+		i++;
+	game->x = i;
+	line = 1;
+	while (m_str[i + 1] != '\0')
+	{
+		if (m_str[i] == '\n')
+			line++;
+		i++;
+	}
+	game->y = line;
+}
+
+void ft_init_map(t_game *game, char *m_str)
+{
+	int		i;
+	int		idx;
+	char	*tmp;
+
+	ft_init_map_xy(game, m_str);
+	game->map = malloc((game->y + 1) * sizeof(char *));
+	if (game->map == NULL)
+		exit(1);
+	i = 0;
+	tmp = &m_str[i];
+	while (m_str[i] != '\0')
+	{
+		if (m_str[i] == '\n')
+		{
+			m_str[i] = '\0';
+			game->map[idx] = ft_strdup(tmp);
+			idx++;
+			tmp = &m_str[i + 1];
+		}
+		i++;
+	}
+	game->map[idx] = NULL;
+}
+
+t_game *ft_set_game(char *m_str)
+{
+	t_game *game;
+	
+	game = malloc(sizeof(t_game));
+	if (game == NULL)
+		return (NULL);
+	ft_init_map(game, m_str);
+	return (game);
+}
+
+// start using mlx
+//
+
+void ft_draw_player(t_game *game)
+{
+}
+
+void ft_draw_screen(t_game *game)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (game->map[i] != NULL)
+	{
+		j = 0;
+		while (game->map[i][j] != '\0')
+		{
+			if (game->map[i][j] == '1')
+				mlx_put_image_to_window(game->mlx_ptr, game->mlx_win,
+						game->tex_wall.img, j * TILE_SIZE, i * TILE_SIZE);
+			else if (game->map[i][j] == '0')
+				mlx_put_image_to_window(game->mlx_ptr, game->mlx_win,
+						game->tex_tile.img, j * TILE_SIZE, i * TILE_SIZE);
+			else if (game->map[i][j] == 'P')
+				mlx_put_image_to_window(game->mlx_ptr, game->mlx_win,
+						game->tex_player.img, j * TILE_SIZE, i * TILE_SIZE);
+			j++;
+		}
+		i++;
+	}
+}
+
+int ft_game_loop(t_game *game)
+{
+	ft_draw_screen(game);
+	return (0);
+}
+
+void ft_save_texture(t_game *game, t_img img)
+{
+	int x;
+	int y;
+
+	y = 0;
+	while (y < TILE_SIZE)
+	{
+		x = 0;
+		while (x < TILE_SIZE)
+		{
+			game->texture_wall[y][x] = img.data[(y * TILE_SIZE) + x];
+			x++;
+		}
+		y++;
+	}
+}
+
+void ft_player_texture(t_game *game)
+{
+	game->tex_player.img = mlx_xpm_file_to_image(game->mlx_ptr, "./texture/p_s.xpm",
+			&(game->tex_player.width), &(game->tex_player.height));
+	game->tex_player.data = (int *)mlx_get_data_addr(game->tex_player.img,
+			&game->tex_player.bpp, &game->tex_player.size_l, &game->tex_player.endian);
+}
+
+void ft_load_texture(t_game *game)
+{
+	game->tex_wall.img = mlx_xpm_file_to_image(game->mlx_ptr, "./texture/wall.xpm",
+			&(game->tex_wall.width), &(game->tex_wall.height));
+	game->tex_wall.data = (int *)mlx_get_data_addr(game->tex_wall.img,
+			&game->tex_wall.bpp, &game->tex_wall.size_l, &game->tex_wall.endian);
+//	ft_save_texture(game, game->tex_wall);
+	game->tex_tile.img = mlx_xpm_file_to_image(game->mlx_ptr, "./texture/tile.xpm",
+			&(game->tex_tile.width), &(game->tex_tile.height));
+	game->tex_tile.data = (int *)mlx_get_data_addr(game->tex_tile.img,
+			&game->tex_tile.bpp, &game->tex_tile.size_l, &game->tex_tile.endian);
+//	ft_save_texture(game, game->tex_tile);
+	ft_player_texture(game);
+}
+
+void ft_init_game(t_game *game)
+{
+	game->mlx_ptr = mlx_init();
+	game->mlx_win = mlx_new_window(game->mlx_ptr, TILE_SIZE * game->x, TILE_SIZE * game->y, "SO_LONG");
+	ft_load_texture(game);
+//	ft_player_position(game);
+}
+
+void ft_start_game(t_game *game)
+{
+	ft_init_game(game);
+	mlx_loop_hook(game->mlx_ptr, &ft_game_loop, game);
+	mlx_loop(game->mlx_ptr);
 }
 
 void ft_game(char *map_file)
 {
 	t_map *map;
 	t_game *game;
-	map = ft_map_parse(map_file);
+	map = ft_map_validation(map_file);
 	if (map == NULL)
 	{
 		printf("malloc fail\n");
 		exit(1);
 	}
-	
+	game = ft_set_game(map->m_str);
+	if (game == NULL)
+		exit(1);
+	ft_start_game(game);
 }
 
 int main(int argc, char *argv[])
--- so_long/so_long.h
+++ so_long/so_long.h
@@ -7,6 +7,50 @@
 # include <fcntl.h>
 # include "../mlx/mlx.h"
 
+# define TILE_SIZE 32
+
+typedef struct s_map
+{
+	char *m_str;
+}	t_map;
+
+typedef struct s_img
+{
+	void	*img; // init s
+	void	*img_n;
+	void	*img_e;
+	void	*img_w;
+	int		*data;
+	int		size_l;
+	int		bpp;
+	int		endian;
+	int		width;
+	int		height;
+}	t_img;
+
+typedef struct s_game
+{
+	int		x;
+	int		y;
+	char	**map;
+
+	int		p_x;
+	int		p_y;
+	void	*mlx_ptr;
+	void	*mlx_win;
+
+	t_img	tex_wall;
+	t_img	tex_tile;
+	t_img	tex_player;
+	t_img	tex_collect;
+	t_img	tex_exit;
+	int		texture_wall[TILE_SIZE][TILE_SIZE];
+	int		texture_tile[TILE_SIZE][TILE_SIZE];
+	int		texture_player[TILE_SIZE][TILE_SIZE];
+	int		texture_collect[TILE_SIZE][TILE_SIZE];
+	int		texture_exit[TILE_SIZE][TILE_SIZE];
+}	t_game;
+
 int ft_strlen(char *str);
 char *ft_strdup(char *str);
 char *ft_strjoin(char *s1, char *s2);
