580cea81f266f1108e27c287f62b50ea55025b0d
gejo <gejo@gejoui-iMac.local>
2021-07-15 21:07:40 +0900

    del_texture

--- so_long/parsing.c
+++ so_long/parsing.c
@@ -6,7 +6,7 @@
 /*   By: gejo <gejo@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/11 23:34:14 by gejo              #+#    #+#             */
-/*   Updated: 2021/07/15 00:12:55 by gejo             ###   ########.fr       */
+/*   Updated: 2021/07/15 20:37:05 by gejo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -518,11 +518,11 @@ int ft_game_loop(t_game *game)
 		ft_draw_sprite(game);
 		ft_draw_exit(game);
 		ft_draw_player(game);
-		ft_move_enemy(game);
-		ft_draw_enemy(game);
+//		ft_move_enemy(game);
+//		ft_draw_enemy(game);
 		ft_game_status(game);
 	}
-	ft_enemy_count_manager(game);
+//	ft_enemy_count_manager(game);
 	return (0);
 }
 
@@ -702,13 +702,13 @@ void ft_game(char *map_file)
 	t_game *game;
 	if (ft_file_name(map_file) != 1)
 	{
-		printf("file_name must .ber\n");
+		ft_putstr_fd("Error\nfile name must end .ber\n", 2);
 		exit(1);
 	}
 	map = ft_map_validation(map_file);
 	if (map == NULL)
 	{
-		printf("malloc fail\n");
+		ft_putstr_fd("Error\nmalloc fail\n", 2);
 		exit(1);
 	}
 	game = ft_set_game(map->m_str);
